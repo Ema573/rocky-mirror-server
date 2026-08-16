08c652fdf8d8f612fcc9b0daa07ba20393206e10
eml <eml@localhost.localdomain>
2026-08-16 22:45:41 +0200

    handling improvement

--- rocky_mirror_set.sh
+++ rocky_mirror_set.sh
@@ -13,11 +13,13 @@
 
 download_repo() {
     printf "Downloading full repo."
+    printf "Repo %s" "${repo_id}"
+    printf "Repo size" dnf repolist -v --repo=baseos | awk -F': ' '/Repo-size/ {print $2}'
     dnf repolist -v --repo="${repo_id}" | grep Repo-available-pkgs
     #dnf reposync -v --repo="${repo_id}" --download-metadata --download-path="${LOCATION}
     # Il reacticvate this once i am done this is just a test
     if [[ "$?" -eq 0 ]]; then
-        printf "All files downloaded. Repo sync finished succsefully!"
+        printf "All files downloaded. Repo sync finished succsefully!\n"
         return 0
     else
         return 1
@@ -32,7 +34,15 @@ printf "\n"
 printf "Select what you want to mirror:\n"
 printf "[1]Mirror existing repo\n"
 printf "[2]Add custom repo\n"
-read -p "Enter:" mirror_decission
+
+while true; do
+    read -p "Enter:" mirror_decission
+    if [[ "${mirror_decission}" -lt 1 || "${mirror_decission}" -gt 2  ]]; then
+        printf "%s invalid input. Try again\n" "$mirror_decission"
+        continue
+    fi
+    break
+done
 
 if [[ "$mirror_decission" -eq 1 ]]; then
     read -p "Enter the repo ID you want to mirror: " repo_id
@@ -40,17 +50,14 @@ if [[ "$mirror_decission" -eq 1 ]]; then
     while [[ -z "$validatingRepoID" ]]; do
         printf "Provided RepoID %s is not available.\n" "${repo_id}"
         read -p "Enter the repo ID you want to mirror: " repo_id
-        printf "Looking up for %s" "$repo_id"
+        printf "Looking up for %s\n" "$repo_id"
         validatingRepoID=$(dnf repolist -v | awk -F': ' '/Repo-id/ {print $2}' | grep -x "${repo_id}")
     done
     printf "%s founded as in the available repos.\n" "${repo_id}"
-else
+elif [[ "${mirror_decission}" -eq 2 ]]; then
     printf "Feature not yet implemented\n"
 fi
 
-if [[ "$mirror_decission" -eq 2 ]]; then
-    printf "This is for cusom entry\n"
-fi
 
 read -p "Enter the location you want to save downloaded files:" LOCATION
 
@@ -65,7 +72,6 @@ REPOSIZE=$(dnf repolist -v --repo="${repo_id}" | grep Repo-size | awk '{print $3
 read -r -p "Do you want to download the full repository? Size: ${REPOSIZE}. [y/n]: " answer
 
 if [[ "$answer" == "y" ]]; then    
-    download_repo
     while ! download_repo; do
         read -r -p "Download failed. Repeat [y/n]: " answer
             if [[ "$answer" != "y" ]]; then
